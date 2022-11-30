5567e52ae0ccc0f4450c7a8936a4d84f506bd987
Facundo <facundogds@gmail.com>
2022-11-30 20:26:12 +0000

    Fix bootstrap details on card

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,8 +2,11 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+import { Card } from "../component/Card.jsx";
+
 export const Home = () => (
 	<div className="text-center mt-5">
+		<Card/>
 		<h1>StarWars Blog Reading List</h1>
 	</div>
 );
